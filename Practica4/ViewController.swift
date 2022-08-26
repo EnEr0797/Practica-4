cd36d21b30a19eee85ccfad488ba5bd4e6be0103
Luis Enrique <luis.enrique.erazo.tescha@gmail.com>
2022-08-26 10:10:55 -0500

    Initial Commit

---
+++ Practica4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practica4
+//
+//  Created by Luis Enrique Erazo Amador on 26/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Practica4UITests/Practica4UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Practica4UITestsLaunchTests.swift
+//  Practica4UITests
+//
+//  Created by Luis Enrique Erazo Amador on 26/08/22.
+//
+
+import XCTest
+
+class Practica4UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
