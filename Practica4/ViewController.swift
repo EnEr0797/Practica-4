d4d41b724eb66da6acc188d29499d0e50c22164f
Luis Enrique <luis.enrique.erazo.tescha@gmail.com>
2022-08-28 13:48:01 -0500

    Se agrega la primera parte de la practica (Maquetación)

--- Practica4/ViewController.swift
+++ Practica4/ViewController.swift
@@ -1,19 +1,27 @@
 //
 //  ViewController.swift
 //  Practica4
-//
 //  Created by Luis Enrique Erazo Amador on 26/08/22.
 //
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var userImg: UIImageView!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        /*Agrega bordes a userImg
+         userImg.layer.borderWidth = 2 */
+        userImg.layer.borderColor = UIColor.black.cgColor
+        userImg.layer.cornerRadius = userImg.frame.size.width/2
+        userImg.clipsToBounds = true
     }
 
-
+    
 }
 
